9e0517a6ed6961294b70d921339b343399cb2a12
Temitope <ojotemitopeidnoble10@gmail.com>
2022-03-29 22:01:21 -0700

    aaa

--- src/screens/PageTwo/PageTwo.js
+++ src/screens/PageTwo/PageTwo.js
@@ -217,7 +217,7 @@ export default function PageTwo({ navigation }) {
                   size={40}
                   color="#214d77"
                   iconStyle={{}}
-                  onPress={() => navigation.navigate('pagetwozfa')}
+                  onPress={() => navigation.navigate('pagetwoza')}
               />
           </TouchableOpacity>
           <Modal
